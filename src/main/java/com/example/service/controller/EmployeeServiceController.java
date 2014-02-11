b770db86431db262efb25fcf9c779be974ba5713
Sachin Bhosale <sbhosale@xpanxion.co.in>
2014-02-11 19:43:18 +0530

    Code update

--- src/main/java/com/example/service/controller/EmployeeServiceController.java
+++ src/main/java/com/example/service/controller/EmployeeServiceController.java
@@ -10,7 +10,6 @@ import javax.ejb.Stateless;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
